7f402d3998c377b1daa269077a449632b1fc8eda
Nick980 <62293831+Nick980@users.noreply.github.com>
2020-03-18 15:12:54 +0000

    First Commit

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// test 1
+let suspect1 = 'Stuart';
+let suspect2 = 'Liam';
+let suspect3 = 'Leona';
+let suspect4 = 'Dean';
+const deadBody = 'Jay';
+
+let playerName;
+let beginGame;
+
+
+const startGame = () => {
+let playerName = prompt("Please enter your name: ");
+// if(playerName.length <= 2) {
+// alert(`Name to Short`);
+// startGame();
+// }
+
+if(playerName == deadBody) {
+    alert(`Sorry Jay is dead, please pick another name`);
+    startGame();
+} else {
+    if(playerName == suspect1) {
+        suspect1 = 'Bob';
+    } else {
+        if(playerName == suspect2) {
+            suspect2 = 'Bob';
+        } else {
+            if(playerName == suspect3) {
+                suspect3 = 'Bob';
+            } else {
+                if(playerName == suspect4) {
+                    suspect4 = 'Bob';
+                }
+            }
+        }
+    }
+}
+
+alert(`${suspect1} ${suspect2} ${suspect3} ${suspect4}`)
+alert(`Hello, ${playerName} Welcome to a Murder at Code Nation`);
+let beginGame = prompt(`Shall we begin? [yes/no]`);
+if(beginGame.toLowerCase == "no") {
+    alert(`Screw you ${playerName}`);
+} else {
+    alert(`test`);
+}
+}
