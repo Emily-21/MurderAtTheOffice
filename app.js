dda8f4678b755cc50bace1db69bf3d3a6dffcd16
Nick980 <62293831+Nick980@users.noreply.github.com>
2020-03-18 16:10:45 +0000

    Submit two

--- app.js
+++ app.js
@@ -4,44 +4,60 @@ let suspect3 = 'Leona';
 let suspect4 = 'Dean';
 const deadBody = 'Jay';
 
+let enterName;
 let playerName;
 let beginGame;
 
-
-const startGame = () => {
-let playerName = prompt("Please enter your name: ");
-// if(playerName.length <= 2) {
-// alert(`Name to Short`);
-// startGame();
-// }
-
-if(playerName == deadBody) {
-    alert(`Sorry Jay is dead, please pick another name`);
-    startGame();
+const nameCheck = () => {
+enterName = prompt("Please enter your name: ");
+playerName = enterName.charAt(0).toUpperCase();
+playerName = playerName + enterName.substring(1);
+// playerName = enterName.charAt(0).toUppperCase() + enterName.substring(1);
+if(playerName.length <= 2) {
+alert(`Name to Short`);
+nameCheck();
 } else {
-    if(playerName == suspect1) {
-        suspect1 = 'Bob';
+    if(playerName == deadBody) {
+        alert(`Sorry Jay is dead, please pick another name`);
+        startGame();
     } else {
-        if(playerName == suspect2) {
-            suspect2 = 'Bob';
+        if(playerName == suspect1) {
+            suspect1 = 'Bob';
         } else {
-            if(playerName == suspect3) {
-                suspect3 = 'Bob';
+            if(playerName == suspect2) {
+                suspect2 = 'Bob';
             } else {
-                if(playerName == suspect4) {
-                    suspect4 = 'Bob';
+                if(playerName == suspect3) {
+                    suspect3 = 'Bob';
+                } else {
+                    if(playerName == suspect4) {
+                        suspect4 = 'Bob';
+                    }
                 }
             }
         }
     }
 }
+}
 
-alert(`${suspect1} ${suspect2} ${suspect3} ${suspect4}`)
-alert(`Hello, ${playerName} Welcome to a Murder at Code Nation`);
-let beginGame = prompt(`Shall we begin? [yes/no]`);
-if(beginGame.toLowerCase == "no") {
+const shallWe = () => {
+beginGame = prompt(`Shall we begin? [yes/no]`);
+if(beginGame.toLowerCase() == "no") {
     alert(`Screw you ${playerName}`);
 } else {
+    if(beginGame.toLowerCase() == "yes") {
     alert(`test`);
+    } else {
+        if(beginGame != 'no' && beginGame != 'yes');
+        alert(`Please Enter yes or no`)
+        shallWe();
+    }
 }
+}
+
+const startGame = () => {
+nameCheck();
+alert(`${suspect1} ${suspect2} ${suspect3} ${suspect4}`)
+alert(`Hello, ${playerName} Welcome to a Murder at Code Nation`);
+shallWe();
 }
